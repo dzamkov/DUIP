ad629915f133532f14f21081fbf9baea07a03b62
dzamkov <dzamkov@xiatek.org>
2011-04-01 21:09:16 -0400

    Restarting

--- Program.cs
+++ Program.cs
@@ -1,57 +1,25 @@
-﻿//***********************************
-// Copyright (c) 2010, Dmitry Zamkov 
-// Open source under the BSD License 
-//***********************************
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Windows.Forms;
-
-using OpenTK;
-
-using DUIP.Net;
 
 namespace DUIP
 {
-    static class Program
+    /// <summary>
+    /// Contains functions and information related to the program.
+    /// </summary>
+    public static class Program
     {
         /// <summary>
-        /// The main entry point for the application.
+        /// Program release version number.
         /// </summary>
-        [STAThread]
-        static void Main()
+        public const int Version = 1;
+
+        /// <summary>
+        /// Program main entry point.
+        /// </summary>
+        public static void Main(string[] Args)
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
 
-            Core.TypeDirectory.LoadAssembly(typeof(Program).Assembly);
-            DialogResult dr = MessageBox.Show("Wanna run Server(Okay) or Client(Cancel)?", "Honest question", MessageBoxButtons.OKCancel);
-            bool part = dr == DialogResult.Cancel;
-
-            UDPConnection con = new UDPConnection(27200 + (part ? 1 : 0));
-            NetManager man;
-            if (part)
-            {
-                System.Net.IPEndPoint endpoint = new System.Net.IPEndPoint(System.Net.IPAddress.Loopback, 27200);
-                man = new NetManager(con, con, null);
-                man.Connect(endpoint);
-            }
-            else
-            {
-                Core.World world = new Core.World();
-                man = new NetManager(con, con, world);
-            }
-
-            Visual.Window win = new Visual.Window(DisplayDevice.Default);
-            if (part)
-            {
-                win.Title = "DUIP Client";
-            }
-            else
-            {
-                win.Title = "DUIP Server";
-            }
-            win.Run(60.0);
         }
     }
 }
