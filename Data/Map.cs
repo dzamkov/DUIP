6b467d4f80f80beeafa4fae65d3f76070ea9d497
dzamkov <dzamkov@xiatek.org>
2011-04-23 12:08:37 -0400

    TransferMap

--- Data/Map.cs
+++ Data/Map.cs
@@ -73,4 +73,79 @@ namespace DUIP
             }
         }
     }
+
+    /// <summary>
+    /// A partial map that tries transfering items between two mutable partial maps over time and with usage.
+    /// </summary>
+    public class TransferMap<TKey, T> : PartialMap<TKey, T>
+    {
+        public TransferMap(PartialMap<TKey, T> Primary, PartialMap<TKey, T> Secondary)
+        {
+            this._Primary = Primary;
+            this._Secondary = Secondary;
+        }
+
+        public override bool Immutable
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public override Maybe<T> Lookup(TKey Key)
+        {
+            Maybe<T> pri = this._Primary.Lookup(Key);
+            Maybe<T> sec = this._Secondary.Lookup(Key);
+            if (!sec.HasValue)
+            {
+                return pri;
+            }
+            if (!pri.HasValue)
+            {
+                if (this._Primary.Modify(Key, pri = sec))
+                {
+                    this._Secondary.Remove(Key);
+                }
+            }
+            else
+            {
+                this._Secondary.Remove(Key);
+            }
+            return pri;
+        }
+
+        public override bool Modify(TKey Key, Maybe<T> Value)
+        {
+            this._Secondary.Remove(Key);
+            return this._Primary.Modify(Key, Value);
+        }
+
+        /// <summary>
+        /// Gets the primary map, the map to which items are transfered. All new items will be added to
+        /// this map.
+        /// </summary>
+        public PartialMap<TKey, T> Primary
+        {
+            get
+            {
+                return this._Primary;
+            }
+        }
+
+        /// <summary>
+        /// Gets the secondary map, the map from which items are transfered. The amount of items this map has
+        /// will decrease over time as they are transfered to the primary map.
+        /// </summary>
+        public PartialMap<TKey, T> Secondary
+        {
+            get
+            {
+                return this._Secondary;
+            }
+        }
+
+        private PartialMap<TKey, T> _Primary;
+        private PartialMap<TKey, T> _Secondary;
+    }
 }
