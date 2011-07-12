5e9b39a9538d800d536ef504704baf917ed8db4b
DmitryZamkov <dmitry94@gmail.com>
2011-07-12 10:09:57 -0400

    Using a different way to define events

---
+++ Util/Event.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DUIP
+{
+    /// <summary>
+    /// A function that removes a callback added with a Register method. It is possible for remove handlers
+    /// to be null if no process is needed to remove a callback (the callback will never be called).
+    /// </summary>
+    public delegate void RemoveHandler();
+}
